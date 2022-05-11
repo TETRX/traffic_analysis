5af05306944fa6a471d0031cfe65f14ca6887f5e
Wojciech <w.wegrzynek@gmail.com>
2022-05-11 17:57:49 +0200

    delta graphs

--- graph_edit_functions.py
+++ graph_edit_functions.py
@@ -1,5 +1,6 @@
 import osmnx as ox
 import networkx as nx
+import pandas as pd
 from shapely.geometry import Point, LineString
 
 
@@ -23,3 +24,22 @@ def get_center(g, property_dict, threshold=0.1):
 
 def add_edge(g, node1, node2, weight=None):
     g.add_edge(node1,node2, geometry=LineString([Point(g.nodes[node1]["x"], g.nodes[node1]["y"]), Point(g.nodes[node2]["x"], g.nodes[node2]["y"])]), weight=weight)
+
+def _get_nodes_as_pd_df(graph):
+    return pd.DataFrame.from_dict(dict(graph.nodes(data=True)), orient='index')
+
+def get_delta_graph(g_new, g_old, *delta_attributes):
+    df_new = _get_nodes_as_pd_df(g_new)
+    df_old = _get_nodes_as_pd_df(g_old)
+
+    df_new = df_new[df_new.columns.intersection(delta_attributes)]
+    df_old = df_old[df_old.columns.intersection(delta_attributes)]
+
+    delta_df = df_new-df_old
+
+    nodes = delta_df.to_dict('index')
+
+    delta_graph = nx.MultiDiGraph(g_new)
+    nx.set_node_attributes(delta_graph, nodes)
+
+    return delta_graph
--- plot_functions.py
+++ plot_functions.py
@@ -13,7 +13,7 @@ import matplotlib.colors as mcolors
 
 
 
-def basic_plot(G, node_weights, title, path=None, special_edges = None):
+def basic_plot(G, node_weights, title, path=None, special_edges = None, change_sizes=True):
     edge_color="white"
     edge_width = 1
     if special_edges is not None:
@@ -25,11 +25,17 @@ def basic_plot(G, node_weights, title, path=None, special_edges = None):
     
     min_val = min(node_weights.values())
     max_val = max(node_weights.values())
+    print(f"Range: {min_val} - {max_val}")
     node_weights = {key: (val-min_val) / (max_val-min_val) for key, val in node_weights.items()}
 
     nx.set_node_attributes(G, values=node_weights, name=title)
     color_map = ox.plot.get_node_colors_by_attr(G, title, cmap="jet")
-    node_size = [val*50+15 for val in node_weights.values()]
+
+    if change_sizes:
+        node_size = [val*50+15 for val in node_weights.values()]
+    else:
+        node_size = 15
+    
     ox.plot_graph(G, edge_color=edge_color, edge_linewidth=edge_width, node_color=color_map, node_size=node_size, figsize=(55, 55), show=False, save=True, filepath=path)
     if path:
         plt.savefig(path)
