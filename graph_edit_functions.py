bf282bb74f23d07860fe3f55ce8fc4d82ee37a39
Pablo1785 <pablo1785@wp.pl>
2022-06-05 23:29:34 +0200

    Moved get_graph and get_crossroad_points to graph edit funcs file from notebook

--- graph_edit_functions.py
+++ graph_edit_functions.py
@@ -9,13 +9,9 @@ import networkx as nx
 from shapely.geometry import Point, LineString
 
 
-def get_sc(g):
-    scc = nx.strongly_connected_components(g)
-    scc = list(scc)
-
-    main_component = max(scc, key = len)
-
-    return g.subgraph(main_component)
+def add_edge(g: nx.MultiDiGraph, node1, node2, weight=None):
+    g.add_edge(node1, node2, geometry=LineString([Point(g.nodes[node1]["x"], g.nodes[node1]["y"]), Point(g.nodes[
+                                                                                                             node2]["x"], g.nodes[node2]["y"])]), weight=weight)
 
 def get_center(g, property_dict, threshold=0.1):
     max_val = max(property_dict.values())
@@ -50,6 +46,42 @@ def get_delta_graph(g_new, g_old, *delta_attributes):
 def add_edge(g: nx.MultiDiGraph, node1, node2, weight=None):
     g.add_edge(node1, node2, geometry=LineString([Point(g.nodes[node1]["x"], g.nodes[node1]["y"]), Point(g.nodes[
                                                                                                              node2]["x"], g.nodes[node2]["y"])]), weight=weight)
+def get_crossroad_nodes(edges, street_name1: str, street_name2: str) -> Set[int]:
+    """
+    :param edges:
+    :param street_name1: Doesn't need to be the full name, just a fragment of the street name will be enough if its unique, e.x. part of the surname could suffice
+    :param street_name2:
+    :return: Set of all nodes that belong simultaneously to both streets
+    """
+    relevant_edges1 = edges[edges['name'].str.contains(street_name1).fillna(False)]
+    relevant_edges2 = edges[edges['name'].str.contains(street_name2).fillna(False)]
+
+    index1 = relevant_edges1.index.to_list()
+    index2 = relevant_edges2.index.to_list()
+
+    crossroad_nodes = set()
+    for u1, v1, _ in index1:
+        for u2, v2, __ in index2:
+            if u1 in (u2, v2):
+                crossroad_nodes.add(u1)
+            elif v1 in (u2, v2):
+                crossroad_nodes.add(v1)
+    return crossroad_nodes
+
+def get_graph_from_place(place: str):
+    g = ox.graph_from_place(place, network_type="drive")
+    g = get_sc(g)
+    g = nx.MultiDiGraph(g)  # unfreeze: IMPORTANT
+    nodes, edges = ox.graph_to_gdfs(g)
+    return g, nodes, edges
+
+def get_sc(g):
+    scc = nx.strongly_connected_components(g)
+    scc = list(scc)
+
+    main_component = max(scc, key = len)
+
+    return g.subgraph(main_component)
 
 def remove_edge(g: nx.MultiDiGraph, node1, node2):
     g.remove_edge(node1, node2)
